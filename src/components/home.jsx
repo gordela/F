6df88a9c0487842fcc2e9c02eb710a2298b7a3bd
gordela <giorgi.gordiashvili.1@btu.edu.ge>
2019-09-15 23:20:34 +0400

    last

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -18,21 +18,6 @@ class Home extends Component {
               className="carousel slide"
               data-ride="carousel"
             >
-              <ol className="carousel-indicators">
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to={0}
-                />
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to={1}
-                  className="active"
-                />
-                <li
-                  data-target="#carouselExampleIndicators"
-                  data-slide-to={2}
-                />
-              </ol>
               <div className="carousel-inner" role="listbox">
                 {/* Slide One - Set the background image for this slide in the line below */}
                 <div
@@ -43,8 +28,10 @@ class Home extends Component {
                   }}
                 >
                   <div className="carousel-caption d-none d-md-block">
-                    <h3>First Slide</h3>
-                    <p>This is a description for the first slide.</p>
+                    <img
+                      src="http://emg.ge/wp-content/themes/emg/images/cover-logo.png"
+                      alt=""
+                    />
                   </div>
                 </div>
                 {/* Slide Two - Set the background image for this slide in the line below */}
@@ -56,8 +43,10 @@ class Home extends Component {
                   }}
                 >
                   <div className="carousel-caption d-none d-md-block">
-                    <h3>Second Slide</h3>
-                    <p>This is a description for the second slide.</p>
+                    <img
+                      src="http://emg.ge/wp-content/themes/emg/images/cover-logo.png"
+                      alt=""
+                    />
                   </div>
                 </div>
                 {/* Slide Three - Set the background image for this slide in the line below */}
@@ -69,35 +58,13 @@ class Home extends Component {
                   }}
                 >
                   <div className="carousel-caption d-none d-md-block">
-                    <h3>Third Slide</h3>
-                    <p>This is a description for the third slide.</p>
+                    <img
+                      src="http://emg.ge/wp-content/themes/emg/images/cover-logo.png"
+                      alt=""
+                    />
                   </div>
                 </div>
               </div>
-              <a
-                className="carousel-control-prev"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="prev"
-              >
-                <span
-                  className="carousel-control-prev-icon"
-                  aria-hidden="true"
-                />
-                <span className="sr-only">Previous</span>
-              </a>
-              <a
-                className="carousel-control-next"
-                href="#carouselExampleIndicators"
-                role="button"
-                data-slide="next"
-              >
-                <span
-                  className="carousel-control-next-icon"
-                  aria-hidden="true"
-                />
-                <span className="sr-only">Next</span>
-              </a>
             </div>
           </header>
         </div>
